6d289165a950ebaeb8e2283ed4efa907e92139b1
Evan <koopevan@gmail.com>
2022-09-07 11:06:13 -0600

    Added design for delete button

--- client/collection.js
+++ client/collection.js
@@ -7,13 +7,15 @@ const getCollection = () => {
     .then((res) => {
         res.data.forEach(element => {
             let baseballCard = 
-            ` <div class="card">
+            `
+            <div class="spacing">
+             <div class="card">
             <div class="image_url"><img class="photo" src=${element.image_url}/></div>       
             <div class="name"><p>${element.name}</p></div>
             <div class="position">${element.position}</div>
             <div class="team">${element.team}</div>
-            <br>
-            <br>
+            <div><button class="delete">Delete Card</button></div>
+            </div>
             `
             cardList.innerHTML += baseballCard
         })
